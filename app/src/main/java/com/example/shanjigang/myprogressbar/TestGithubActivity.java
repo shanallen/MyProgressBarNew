3e33edfffd6429182941fd1b31e086e22e3bd048
shanallen <shanjiqiang@126.com>
2017-02-26 11:56:44 +0800

    增加了TestGitHubAcitivity

---
+++ app/src/main/java/com/example/shanjigang/myprogressbar/TestGithubActivity.java
@@ -0,0 +1,10 @@
+package com.example.shanjigang.myprogressbar;
+
+import android.app.Activity;
+
+/**
+ * Created by shanjigang on 2017/2/26.
+ */
+
+public class TestGithubActivity extends Activity {
+}
