7763d0a9e7a6780eb2c92b715d85f3dd8a8d8407
shanallen <shanjiqiang@126.com>
2017-02-26 12:24:05 +0800

    添加oncreate()新建分支

--- app/src/main/java/com/example/shanjigang/myprogressbar/TestGithubActivity.java
+++ app/src/main/java/com/example/shanjigang/myprogressbar/TestGithubActivity.java
@@ -1,6 +1,7 @@
 package com.example.shanjigang.myprogressbar;
 
 import android.app.Activity;
+import android.os.Bundle;
 
 /**
  * Created by shanjigang on 2017/2/26.
@@ -8,4 +9,9 @@ import android.app.Activity;
 
 public class TestGithubActivity extends Activity {
 //
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
 }
