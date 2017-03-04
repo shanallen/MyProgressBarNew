9eef60a8f2467541ff78320b6ac0bbf98caabe43
shanallen <shanjiqiang@126.com>
2017-03-04 17:01:32 +0800

    1.添加多版本打包app

---
+++ app/src/main/java/com/example/shanjigang/myprogressbar/StrKey.java
@@ -0,0 +1,10 @@
+package com.example.shanjigang.myprogressbar;
+
+/**
+ * Created by shanjigang on 2017/3/3.
+ */
+
+public class StrKey {
+    public static final String DEVELOP = "dev";//开发环境
+    public static final String PRODUCT = "release";//正式环境
+}
